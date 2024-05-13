b89609cc988e8f125258fea0b0607fc46a7dbcdc
Divy Srivastava <dj.srivastava23@gmail.com>
2024-05-13 22:17:49 +0530

    Add meta tags

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,19 +1,28 @@
 export default {
   title: "Deep Dip 2 community page",
-  head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
+  head: `
+  <meta name="description" content="Trackmania's Deep Dip 2 community leaderboard">
+  <meta name="author" content="Divy Srivastava">
+  <meta name="keywords" content="Deep Dip 2, leaderboard, deepdip2, community page, trackmania, open source">
+  <meta name="robots" content="index, follow">
+  <meta name="revisit-after" content="1 days">
+
+  <meta property="og:title" content="Deep Dip 2 community leaderboard">
+  <meta property="og:description" content="Trackmania's Deep Dip 2 community leaderboard">
+  <meta property="og:image" content="https://paste.divy.work/qP5SzK.png">
+
+  <meta property="twitter:title" content="Deep Dip 2 community leaderboard">
+  <meta property="twitter:description" content="Trackmania's Deep Dip 2 community leaderboard">
+  <meta property="twitter:image" content="https://avatars.githubusercontent.com/u/44036562?v=4">
+
+  <link rel="icon" href="https://paste.divy.work/qP5SzK.png">
+  `,
 
   root: "src",
 
-  // Some additional configuration options and their defaults:
-  // theme: "default", // try "light", "dark", "slate", etc.
+  theme: "dark", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
   // footer: "Built with Observable.", // what to show in the footer (HTML)
-  // sidebar: true, // whether to show the sidebar
-  // toc: true, // whether to show the table of contents
-  // pager: true, // whether to show previous & next links in the footer
-  // output: "dist", // path to the output root for build
-  // search: true, // activate search
-  // linkify: true, // convert URLs in Markdown to links
-  // typographer: false, // smart quotes and other typographic improvements
-  // cleanUrls: true, // drop .html from URLs
+  sidebar: false,
+  typographer: true, // smart quotes and other typographic improvements
 };
